86e267a7b282e4a598497881f5fcbbb6495352c0
Alex Shive <mail@alexshive.com>
2015-04-23 16:30:06 -0700

    Added test image

--- BlurMyPhoto/ViewController.swift
+++ BlurMyPhoto/ViewController.swift
@@ -10,14 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	var userImage: UIImageView!
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+	}
+	
+	override func viewDidAppear(animated: Bool) {
+		super.viewDidAppear(animated)
+		userImage = UIImageView(frame: view.frame)
+		userImage.image = UIImage(named: "test")
+		userImage.contentMode = .ScaleAspectFill
+		view.addSubview(userImage)
+		applyBlurEffect(userImage)
 	}
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
+	}
+	
+	func applyBlurEffect(image: UIImageView){
+		var imageToBlur = CIImage(image: image.image)
+		var blurfilter = CIFilter(name: "CIGaussianBlur")
+		blurfilter.setValue(imageToBlur, forKey: "inputImage")
+		blurfilter.setValue(NSNumber(float: 20), forKey: "inputRadius")
+		var resultImage = blurfilter.valueForKey("outputImage") as CIImage
+		
+		// this stretches the image
+//		var blurredImage = UIImage(CIImage: resultImage)
+		
+		var rect = resultImage.extent()
+		rect.origin.x += (rect.size.width  - userImage.frame.size.width) / 2
+		rect.origin.y += (rect.size.height - userImage.frame.size.height) / 2
+		rect.size = userImage.frame.size
+		
+		var context = CIContext(options: nil)
+		var cgimg = context.createCGImage(resultImage, fromRect: rect)
+		var blurredImage = UIImage(CGImage: cgimg)
+		
+		userImage.image = blurredImage
 	}
 
 
